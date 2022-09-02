bd72ba376c30fd7b42753c801a705d01fe57d91c
OlgaKrzysztofik <olga.krzysztofik@gmail.com>
2022-09-02 18:45:57 +0200

    Sending signup requests from the frontend

--- pages/api/auth/signup.js
+++ pages/api/auth/signup.js
@@ -2,10 +2,13 @@ import { connectToDatabase } from "../../../lib/db";
 import { hashPassword } from "../../../lib/auth";
 
 async function handler(req, res) {
+  if (req.method !== "POST") {
+    return;
+  }
+
   const data = req.body;
 
   const { email, password } = data;
-
   if (
     !email ||
     !email.includes("@") ||
@@ -13,23 +16,23 @@ async function handler(req, res) {
     password.trim().length < 7
   ) {
     res.status(422).json({
-      message: "Invalid input - password should be at least 7 characters long",
+      message:
+        "Invalid input - password should also be at least 7 characters long.",
     });
     return;
   }
-
   const client = await connectToDatabase();
 
   const db = client.db();
 
-  const hashedPassword = hashPassword(password);
+  const hashedPassword = await hashPassword(password);
 
   const result = await db.collection("users").insertOne({
     email: email,
     password: hashedPassword,
   });
 
-  res.status(201).json({ message: "Created user" });
+  res.status(201).json({ message: "Created user!" });
 }
 
 export default handler;
